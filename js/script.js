8991098a644f2e8605f72b15f5bb450c093bbed5
Gpzxy <galuhgaluhpakuan@gmail.com>
2023-06-10 00:31:59 +0700

    benerin navbar

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
+const navbarnav = document.querySelector(".navbar-nav");
 // ketika hamburger menu diklik
 document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+  navbarnav.classList.toggle("active");
 };
 
 // keluar sidebar
 const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
+  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
   }
 });
